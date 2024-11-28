9185cfc383b47b00e1858db19a26ad1b8bdac6a9
Ariel Vasconcelos <ariel.goncalves@sou.unifeob.edu.br>
2024-11-27 21:58:56 -0300

    Att 27/11
    Att 27/11

--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -10,16 +10,28 @@ export default function QueroDoar(){
         <section className={s.section1}>
             <p>Por favor, preencha o formulário com suas informações e as informações do Livro</p>
         </section>
-        <section className={s.section2}>
-            <div>
+        <form className={s.section2}>
+            <section className={s.sectionimg}>
                 <img src={livro} alt="" />
+            </section>
+        <section action="" className={s.section3}>
+            <div>
+                <input type="text" name='' id='' placeholder='Título' />
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <div>
+                <input type="text" name='' id='' placeholder='Categoria' />
+            </div>
+            <div>
+                <input type="text" name='' id='' placeholder='Autor' />
+            </div>
+            <div>
+                <input type="text" name='' id='' placeholder='Link da Imagem' />
+            </div>
         </section>
+        <section className={s.section4}>
+            <button type='Submit'>Doar</button>
+        </section>
+        </form>
         </main>
     );
 
