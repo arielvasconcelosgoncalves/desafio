6f79125858f481b470253eebadf8fa1df469dbbf
Ariel Vasconcelos <ariel.goncalves@sou.unifeob.edu.br>
2024-12-01 16:06:20 -0300

    Att 01/12
    Atualização Inicio Responsivo

--- src/assets/components/Footer/footer.jsx
+++ src/assets/components/Footer/footer.jsx
@@ -12,14 +12,18 @@ export default function Footer() {
     return (
       <footer className={s.foot}>
         <section className={s.footer}>
-          <div>
+          <section className={s.sectiondiv}>
+            <div className={s.divtxt}>
             <p>4002-8922</p>
+            </div>
+            <div className={s.divimg}>
             <img src={facebook} alt="icone facebook" className={s.icone1} />
             <img src={twitter} alt="icone twitter" className={s.icone2} />
             <img src={youtube} alt="icone youtube" className={s.icone3} />
             <img src={linkedin} alt="icone linkedin" className={s.icone4} />
             <img src={instagram} alt="icone instagram" className={s.icone5} />
-          </div>
+            </div>
+          </section>
         </section>
         <section className={s.footer2}>
           <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024  </p>
